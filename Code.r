7f73a80824326c5828db05b9a614446d4c5b7b8f
SuryaMurali <smurali7@ncsu.edu>
2016-11-17 10:44:57 -0500

    Update Code.r

--- Code.r
+++ Code.r
@@ -1,6 +1,7 @@
 file <- read.csv(//location//file.csv, Header = T) \\file has all data points measured so far
-ifelse(nrow(file)>250, data<-file[c(nrow(file)-249, nrow(file)),], data<-file)
-while(TRUE)
+ifelse(nrow(file)>250, data<-file[c(nrow(file)-248:nrow(file)),], data<-file)
+count <- 2000 // no. of iterations needed
+for ( i in 1:count)
 {
 Sys.sleep(15)
 if(class(read.csv(//location//data.csv)) != "try-error")
@@ -8,6 +9,10 @@ if(class(read.csv(//location//data.csv)) != "try-error")
     new <- read.csv(//location//new.csv, header = T)
     data <- data[-1,]
     data[250,] <- new[2,]
+    }
+else
+    {
+    next
     }
 x <- BoxCox.lambda(data[,ncol(data)]) //renders data homoscedastic
 
@@ -25,3 +30,4 @@ if(nd >0)
 model <- auto.arima(xstar)
 result <- forecast(model)
 write.table(data.frame(c(2),result),"export.csv", row.names=F,na="NA",append=T, quote= FALSE, sep=",", col.names=F)
+}
